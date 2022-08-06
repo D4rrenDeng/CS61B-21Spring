0f3894ef8755086ee10e21a2cb00c62904a83d2d
D4rrenDeng <darrent211@gmail.com>
2022-08-05 22:37:25 -0400

    lab1 finished

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,11 +1,11 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Darren Deng
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n  == 1) {
             return 1;
         } else if (n % 2 != 0) {
             return 3 * n + 1;
